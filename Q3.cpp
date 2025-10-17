1f323324bd74e75b53455a14d3ffea95f6211e06
mems250005007-netizen <mems250005007@iiti.ac.in>
2025-10-17 16:39:03 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,26 @@
+       
+#include <iostream>
+using namespace std;
 
+struct Point {float x; float y;};
+
+int main() {
+    int c=0;
+    Point items[7] = { };
+    cout << "Enter coordinates for 7 points (x, y):" << endl;
+    
+    
+    
+    for (int i = 0; i < 7; i++) {
+        cout<< "Point " << i+1 <<"- x: ";
+        cin >> items[i].x;
+        cout << endl;
+        cout<< "Point " << i+1 <<"- y: ";
+        cin >> items[i].y;
+        cout << endl ;
+        
+        
+        
+        
+    }
+    
